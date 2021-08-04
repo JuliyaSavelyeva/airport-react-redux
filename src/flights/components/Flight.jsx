c2385cacc753b8b877d3ce9aa8406b9417d04275
JuliyaSavelyeva <juliyasavelyeva0605@gmail.com>
2021-08-04 09:57:42 +0300

    add

---
+++ src/flights/components/Flight.jsx
@@ -0,0 +1,61 @@
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import moment from 'moment';
+import '../../table.scss';
+import { useLocation } from 'react-router-dom';
+
+const Flight = ({ flight }) => {
+  const { pathname } = useLocation();
+  const [time, setTime] = useState({
+    localTime: null,
+    status: null,
+  });
+
+  const getTime = data => moment(new Date(data)).format('H:mm');
+
+  useEffect(() => {
+    const arrivalStatus = flight.timeLandFact
+      ? getTime(flight.timeLandFact)
+      : getTime(flight.timeLandCalc);
+
+    const departureStatus = flight.timeTakeofFact
+      ? getTime(flight.timeTakeofFact)
+      : getTime(flight.timeDepExpectCalc);
+
+    const localeTimeArrival = getTime(flight.timeToStand);
+    const localeTimeDeparture = getTime(flight.timeDepShedule);
+    setTime({
+      localTime: pathname === '/departure' ? localeTimeDeparture : localeTimeArrival,
+      status: pathname === '/departure' ? departureStatus : arrivalStatus,
+    });
+  }, []);
+
+  const terminalName =
+    flight.term === 'A' ? 'terminal-name terminal-name_green' : 'terminal-name terminal-name_blue';
+
+  return (
+    <tr className="table-row">
+      <td className="terminal-field table-cell">
+        <span className={terminalName}>{flight.term}</span>
+      </td>
+      <td className="time-field">{time.localTime}</td>
+      <td className="city-field">
+        {flight['airportToID.city_en']}
+        {flight['airportFromID.city_en']}
+      </td>
+      <td className="status-field">{time.status}</td>
+      <td className="company-field">
+        {flight.airline.logoName}
+        <img src={flight.airline.en.logoSmallName} width="60" height="40" alt="companyName" />
+        <p>{flight.airline.en.name}</p>
+      </td>
+      <td className="flight-field">{flight.codeShareData[0].codeShare}</td>
+    </tr>
+  );
+};
+
+Flight.propTypes = {
+  flight: PropTypes.shape().isRequired,
+};
+
+export default Flight;
--- src/flights/components/FlightsList.jsx
+++ src/flights/components/FlightsList.jsx
@@ -1,10 +1,9 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import moment from 'moment';
+import Flight from './Flight.jsx';
 import '../../table.scss';
-import { useLocation } from 'react-router-dom';
 
-const FlightsList = ({ flightsList, direction }) => {
+const FlightsList = ({ flightsList }) => {
   return (
     <table className="table">
       <thead>
@@ -18,54 +17,9 @@ const FlightsList = ({ flightsList, direction }) => {
         </tr>
       </thead>
       <tbody>
-        {flightsList.map(flight => {
-          const getTime = data => moment(new Date(data)).format('H:mm');
-          const localeTimeArrival = getTime(flight.timeToStand);
-          const localeTimeDeparture = getTime(flight.timeDepShedule);
-
-          const arrivalStatus = flight.timeLandFact
-            ? getTime(flight.timeLandFact)
-            : flight.timeLandCalc && getTime(flight.timeLandCalc);
-          const departureStatus = flight.timeTakeofFact
-            ? getTime(flight.timeTakeofFact)
-            : flight.timeDepExpectCalc && getTime(flight.timeDepExpectCalc);
-
-          const terminalName =
-            flight.term === 'A'
-              ? 'terminal-name terminal-name_green'
-              : 'terminal-name terminal-name_blue';
-
-          return (
-            <tr className="table-row" key={flight.ID}>
-              <td className="terminal-field table-cell">
-                <span className={terminalName}>{flight.term}</span>
-              </td>
-              <td className="time-field">
-                {direction === 'arrival' && localeTimeArrival}
-                {direction === 'departure' && flight.timeDepShedule && localeTimeDeparture}
-              </td>
-              <td className="city-field">
-                {flight['airportToID.city_en']}
-                {flight['airportFromID.city_en']}
-              </td>
-              <td className="status-field">
-                {direction === 'departure' && departureStatus}
-                {direction === 'arrival' && arrivalStatus}
-              </td>
-              <td className="company-field">
-                {flight.airline.logoName}
-                <img
-                  src={flight.airline.en.logoSmallName}
-                  width="60"
-                  height="40"
-                  alt="companyName"
-                />
-                <p>{flight.airline.en.name}</p>
-              </td>
-              <td className="flight-field">{flight.codeShareData[0].codeShare}</td>
-            </tr>
-          );
-        })}
+        {flightsList.map(flight => (
+          <Flight key={flight.ID} flight={flight} />
+        ))}
       </tbody>
     </table>
   );
@@ -73,7 +27,6 @@ const FlightsList = ({ flightsList, direction }) => {
 
 FlightsList.propTypes = {
   flightsList: PropTypes.arrayOf(PropTypes.shape()).isRequired,
-  direction: PropTypes.string.isRequired,
 };
 
 export default FlightsList;
